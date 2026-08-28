162d565a3ecf63874ee52a858b2c4927f37df0b9
Siiri Virtanen <siiri.e.virtanen@gmail.com>
2026-08-28 13:40:25 +0300

    arvonta toimii

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header.jsx' ;
+import ExerciseForm from './components/excerciseForm.jsx';
+import Results from './components/results.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [selectedExercises, setSelectedExercises] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ExerciseForm onExercisesSelected={setSelectedExercises} />
+      <Results exercises={selectedExercises} />
     </div>
   );
 }
